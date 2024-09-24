2fb3c66d3a185db370dba0c03708779696814a60
Joe Blau <im@joeblau.com>
2024-09-23 18:08:01 -0700

    feat: add first two fucntions

--- src/AAVEView.sol
+++ src/AAVEView.sol
@@ -1,1 +1,14 @@
-contract AAVEView {}
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {Balance, Type, TokenSet, Action, Name} from "./Shared/TokenSet.sol";
+
+contract AAVEView {
+    function getDeposits(address publicAddress, address tokenAddress) public view returns (TokenSet[] memory) {
+        return new TokenSet[](0);
+    }
+
+    function getLoans(address publicAddress, address tokenAddress) public view returns (TokenSet[] memory) {
+        return new TokenSet[](0);
+    }
+}
